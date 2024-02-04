e74ef6695248b4d9cab7d1610770ae766cbb9989
Tanjir <tanjirhc@gmail.com>
2024-02-04 14:06:27 +0600

    Normal Date Time Testing

--- Module-1Console/Program.cs
+++ Module-1Console/Program.cs
@@ -1,11 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] arrStr = { "ASP", "MVC", "CORE" };
 
-int i = 0;
-foreach (string item in arrStr)
-{
-    Console.WriteLine("Item no: " + (i+1).ToString()+" :"+item);
-    i++;
-}
+Console.WriteLine(DateTime.Today.Date.ToString());
+
 
 Console.ReadLine();
