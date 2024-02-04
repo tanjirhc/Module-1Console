350961fc70d9ff7f28f75572543e51b0af5505ec
Tanjir <tanjirhc@gmail.com>
2024-02-04 14:18:51 +0600

    dd/MM/yyyy format

--- Module-1Console/Program.cs
+++ Module-1Console/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(DateTime.Today.Date.ToString());
+Console.WriteLine(DateTime.Today.Date.ToString("dd/MM/yyyy"));
 
 
 Console.ReadLine();
