6fca305b85f1932625151b81ea4aa80b7469bee6
Tanjir <tanjirhc@gmail.com>
2024-02-04 16:08:38 +0600

    Bring Correct time according to system

--- Module-1Console/Program.cs
+++ Module-1Console/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//Console.WriteLine(DateTime.Today.Date.ToString("dd/MM/yyyy"));
+Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
 
-DateTime ptoday = DateTime.ParseExact("04/02/2024", "dd/MM/yyyy", null);
-Console.WriteLine(ptoday.ToString());
+//DateTime ptoday = DateTime.ParseExact("04/02/2024", "dd/MM/yyyy", null);
+//Console.WriteLine(ptoday.ToString());
 
 
 Console.ReadLine();
