95bbf8a97c95e5f8a482a6f5e7812a7d9e0a3443
Tanjir <tanjirhc@gmail.com>
2024-02-04 14:24:48 +0600

    Converting user input to system input, Parsing them to system and getting them from system to backend. finally in SQL.

--- Module-1Console/Program.cs
+++ Module-1Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(DateTime.Today.Date.ToString("dd/MM/yyyy"));
+//Console.WriteLine(DateTime.Today.Date.ToString("dd/MM/yyyy"));
+
+DateTime ptoday = DateTime.ParseExact("04/02/2024", "dd/MM/yyyy", null);
+Console.WriteLine(ptoday.ToString());
 
 
 Console.ReadLine();
